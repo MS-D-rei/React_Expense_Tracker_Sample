01720725b45a4f318d656a60385062c6a42fab2f
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-09-25 22:00:53 +0900

    first commit

---
+++ src/components/ExpenseItem.tsx
@@ -0,0 +1,20 @@
+import '@/components/ExpenseItem.css';
+// import './ExpenseItem.css'
+
+function ExpenseItem() {
+  const expenseDate = new Date(2022, 9, 24);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 300.0;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // "@": fileURLToPath(new URL('./src', import.meta.url)),
+      // https://vueschool.io/articles/vuejs-tutorials/import-aliases-in-vite/
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
