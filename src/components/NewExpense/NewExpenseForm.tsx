0fc9eb329ee3daf0ace8db72e4b9507b96bbf133
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-09-27 16:16:06 +0900

    add submitHanlder and 2way binding in NewExpenseForm

--- src/components/NewExpense/NewExpenseForm.tsx
+++ src/components/NewExpense/NewExpenseForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import '@/components/NewExpense/NewExpenseForm.css';
 
 function NewExpenseForm() {
@@ -44,17 +44,30 @@ function NewExpenseForm() {
     //   return { ...prevState, enteredDate: event.target.value };
     // });
   };
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newExpenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    }
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
+            value={enteredAmount}
             min="0.01"
             step="0.01"
             onChange={amountChangeHandler}
@@ -64,6 +77,7 @@ function NewExpenseForm() {
           <label>Date</label>
           <input
             type="date"
+            value={enteredDate}
             min="2022-01-01"
             max="2024-12-31"
             onChange={dateChangeHandler}
