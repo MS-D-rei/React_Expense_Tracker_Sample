d3567896e29af5d393895358066fc21fc120015b
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-09-27 09:42:45 +0900

    add useState for trial

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import ExpenseDate from '@/components/Expenses/ExpenseDate';
 import Card from '@/components/UI/Card';
 import '@/components/Expenses/ExpenseItem.css';
@@ -9,15 +10,21 @@ function ExpenseItem(props: {
   amount: number;
   date: Date;
 }) {
+  // useState returns object. first element is the current value, and second element is the setter for it
+  const [ title, setTitle ] = useState(props.title);
+  const clickHandler = () => {
+    setTitle('updated!');
+  };
   return (
     <Card className="expense-item">
       <div>
         <ExpenseDate date={props.date} />
       </div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
