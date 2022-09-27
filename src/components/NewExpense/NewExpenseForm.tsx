e5decbb27e35c48d374e428188a4f77f333dea1a
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-09-27 16:03:16 +0900

    add comment for one state management in NewExpenseForm

--- src/components/NewExpense/NewExpenseForm.tsx
+++ src/components/NewExpense/NewExpenseForm.tsx
@@ -5,15 +5,44 @@ function NewExpenseForm() {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
+  // const [userInputExpense, setUserInputExpense] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
+  // });
 
   const titleChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEnteredTitle(event.target.value);
+    // setUserInputExpense({ /* This code is NOT safe when updating state. Pass funciton to setState like below. */
+    //   ...userInputExpense,
+    //   enteredTitle: event.target.value,
+    // });
+    // https://reactjs.org/docs/hooks-reference.html#usestate
+    // 'If the new state is computed using the previous state, you can pass a function to setState.
+    // The function will receive the previous value, and return an updated value'
+    // setUserInputExpense((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
   };
   const amountChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEnteredAmount(event.target.value);
+    // setUserInputExpense({
+    //   ...userInputExpense,
+    //   enteredAmount: event.target.value,
+    // });
+    // setUserInputExpense((prevState) => {
+    //   return { ...prevState, enteredAmount: event.target.value };
+    // });
   };
   const dateChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEnteredDate(event.target.value);
+    // setUserInputExpense({
+    //   ...userInputExpense,
+    //   enteredDate: event.target.value,
+    // });
+    // setUserInputExpense((prevState) => {
+    //   return { ...prevState, enteredDate: event.target.value };
+    // });
   };
   return (
     <form>
@@ -24,11 +53,21 @@ function NewExpenseForm() {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2022-01-01" max="2024-12-31" onChange={dateChangeHandler} />
+          <input
+            type="date"
+            min="2022-01-01"
+            max="2024-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
